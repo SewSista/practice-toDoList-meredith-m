4fdcb5baab67ae653a3eac3ab7b3e783a679a678
Clover <sewsistacomo@gmail.com>
2026-07-28 20:18:11 -0500

    completed header file

--- vite-to-do-list/src/App.jsx
+++ vite-to-do-list/src/App.jsx
@@ -1,10 +1,13 @@
 import ToDoList from "./components/ToDoList";
+import Header from "./components/Header";
 
 function App() {
  
   return (
     <>
-     <ToDoList /> 
+    <Header /> 
+    <ToDoList /> 
+    
     </>
   )
 }
---
+++ vite-to-do-list/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return ("My To-Do List");
+}
+
+export default Header;
